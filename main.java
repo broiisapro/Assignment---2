38cfd089e7d22402de41521a97637e8d43c98e23
Moksh <moksh.siruvani@gmail.com>
2025-03-05 18:43:33 -0500

    somewhat done with the basics

--- main.java
+++ main.java
@@ -1,7 +1,46 @@
+//NOTE: This is a NEWER version where you need the separate “HelpCode” Class included in your project
+//This imports the required library of commands to access the common
+//utilities and the input and output commands.
+import java.io.*;
+import java.util.*;
+
 public class main {
 
-    public static void main(args string[]) {}
+    /**
+     * For a block of comments
+     */
+    public static void main(String[] args) throws IOException {
+        //Clears the screen on each new run.
+        System.out.flush();
 
-    private static void openFile() {}
+        //Variable used to hold which program to run.
+        int iNumberOfSections = 0;
+        String filename = "";
+        int[][] iGradeBook;
+
+        filename = HelpCode.getString(
+            "Please enter the file name (and path) that will be used.",
+            false,
+            3,
+            -1
+        );
+        while (!checkFile(filename)) {
+            filename = HelpCode.getString(
+                "\nThis file: '" +
+                filename +
+                "' does not exists -- please enter a filename (and path) that exists to be read from.",
+                false,
+                3,
+                -1
+            );
+        }
+    }
+
+    /**
+     * This is used to check if a file already exists.
+     */
+    public static boolean checkFile(String strFileToCheck) throws IOException {
+        File fleTemp = new File(strFileToCheck);
+        return fleTemp.exists();
+    }
 }
-//i dont want to hear it, i was messing around, just testing formatting
