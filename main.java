b3764c40a3b4cc15a717395bde7d401e4deb2ebd
Moksh <moksh.siruvani@gmail.com>
2025-03-05 20:47:39 -0500

    I am gonna cry why is coding so hard this project is giving me a headache and I shouldn't have left this till the last day :.(

--- main.java
+++ main.java
@@ -13,11 +13,19 @@ public class main {
         //Clears the screen on each new run.
         System.out.flush();
 
-        //Variable used to hold which program to run.
-        int iNumberOfSections = 0;
         String filename = "";
-        int[][] iGradeBook;
 
+        int totalSections = 0;
+        int totalMarks = 0;
+        int totalSum = 0;
+        double sumOfAverages = 0;
+        int overallHighest = -1;
+        int overallLowest = 101;
+        int overallA = 0;
+        int overallB = 0;
+        int overallC = 0;
+        int overallD = 0;
+        int overallF = 0;
         filename = HelpCode.getString(
             "Please enter the file name (and path) that will be used.",
             false,
@@ -34,6 +42,109 @@ public class main {
                 -1
             );
         }
+        File inputFile = new File(filename);
+        Scanner input = new Scanner(inputFile);
+
+        String outputFilePath =
+            inputFile.getParent() + "/out-" + inputFile.getName();
+        File outputFile = new File(outputFilePath);
+
+        while (input.hasNextInt()) {
+            totalSections++;
+            int numGrades = input.nextInt();
+            System.out.println(
+                "In section: " +
+                totalSections +
+                " there will be " +
+                numGrades +
+                " piece(s) of data."
+            );
+            int sectionHighest = -1;
+            int sectionLowest = 101;
+            int sectionSum = 0;
+            int sectionA = 0;
+            int sectionB = 0;
+            int sectionC = 0;
+            int sectionD = 0;
+            int sectionF = 0;
+            for (int i = 0; i < numGrades; i++) {
+                if (!input.hasNextInt()) break;
+                int grade = input.nextInt();
+
+                sectionSum += grade;
+                sectionHighest = Math.max(sectionHighest, grade);
+                sectionLowest = Math.min(sectionLowest, grade);
+
+                // Categorize grade
+                if (grade >= 80) sectionA++;
+                else if (grade >= 70) sectionB++;
+                else if (grade >= 60) sectionC++;
+                else if (grade >= 50) sectionD++;
+                else sectionF++;
+            }
+            double sectionAverage = (double) sectionSum / numGrades;
+            sumOfAverages += sectionAverage;
+
+            System.out.printf("Highest : %d%n", sectionHighest);
+            System.out.printf("Lowest : %d%n", sectionLowest);
+            System.out.printf("Average : %.1f%n", sectionAverage);
+            System.out.printf("There were %d A's%n", sectionA);
+            System.out.printf("There were %d B's%n", sectionB);
+            System.out.printf("There were %d C's%n", sectionC);
+            System.out.printf("There were %d D's%n", sectionD);
+            System.out.printf("There were %d F's%n", sectionF);
+            System.out.println();
+
+            // Update overall statistics
+            totalMarks += numGrades;
+            totalSum += sectionSum;
+            overallHighest = Math.max(overallHighest, sectionHighest);
+            overallLowest = Math.min(overallLowest, sectionLowest);
+            overallA += sectionA;
+            overallB += sectionB;
+            overallC += sectionC;
+            overallD += sectionD;
+            overallF += sectionF;
+        }
+        // Compute overall statistics
+        double overallAverage = (double) totalSum / totalMarks;
+        double avgOfAverages = sumOfAverages / totalSections;
+
+        // Print overall results
+        System.out.println("Overall Statistics:");
+        System.out.println("-------------------");
+        System.out.printf("There were %d sections.%n", totalSections);
+        System.out.printf("The overall highest mark was: %d%n", overallHighest);
+        System.out.printf("The overall lowest mark was: %d%n", overallLowest);
+        System.out.printf("There were a total of %d marks.%n", totalMarks);
+        System.out.printf("With an overall average of %.1f%n", overallAverage);
+        System.out.printf(
+            "If you averaged all of the averages, %.1f was the result.%n",
+            avgOfAverages
+        );
+        System.out.printf("There were %d A's%n", overallA);
+        System.out.printf("There were %d B's%n", overallB);
+        System.out.printf("There were %d C's%n", overallC);
+        System.out.printf("There were %d D's%n", overallD);
+        System.out.printf("There were %d F's%n", overallF);
+
+        // Write results to output file
+        writer.printf("Overall Statistics for: '%s'%n", inputFile.getName());
+        writer.printf("Total number of sections: %d%n", totalSections);
+        writer.printf("Highest mark: %d%n", overallHighest);
+        writer.printf("Lowest mark: %d%n", overallLowest);
+        writer.printf("Total number of marks: %d%n", totalMarks);
+        writer.printf("Overall average: %.1f%n", overallAverage);
+        writer.printf("Average of section averages: %.1f%n", avgOfAverages);
+        writer.println("Grade Statistics:");
+        writer.printf("There were %d A's%n", overallA);
+        writer.printf("There were %d B's%n", overallB);
+        writer.printf("There were %d C's%n", overallC);
+        writer.printf("There were %d D's%n", overallD);
+        writer.printf("There were %d F's%n", overallF);
+
+        System.out.println("\nResults written to: " + outputFilePath);
+        input.close();
     }
 
     /**
